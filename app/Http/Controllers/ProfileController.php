fcf15bf0cd923bc2cab0b3ae3c0476c3c006fb67
Suman Regmi <limitlessplaying@gmail.com>
2025-05-21 12:41:20 +0545

    Auth API

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+     public function getUser()
+    {
+        $current_user = auth()->user();
+        return response()->json([
+            'user' => $current_user,
+        ], 200);
+    }
+
+    public function editUser(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => [
+                'nullable',
+                'string',
+                'min:3',
+                'max:255',
+                Rule::unique('users')->ignore(auth()->user()->id)->where(function ($query) use ($request) {
+                    return !is_null($request->name);
+                }),
+            ],
+            'password' => 'nullable|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $current_user = auth()->user();
+        $user = User::where('id', $current_user->id)->first();
+        $user->name = $request->name;
+        // if ($request->profile_picture) {
+        //     $user->profile_picture = $request->profile_picture;
+        // } else {
+        //     $user->profile_picture = auth()->user()->profile_picture;
+        // }
+        if ($request->password) {
+            if (Hash::check($request->old_password, $user->password)) {
+                $user->password = Hash::make($request->password);
+            } else {
+                return response()->json([
+                    'errors' => 'Old Password Did Not match',
+                ], 422);
+            }
+        }
+        $user->update();
+        return response()->json([
+            'message' => 'User Details Updated successfully',
+            'user' => $user,
+        ], 201);
+    }
+
+    public function fetchUser(Request $request)
+    {
+        $users = User::where('email', 'like', '%' . $request->email . '%')->get();
+        return response()->json(['users' => $users]);
+    }
+}
+
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// not logged in user routes
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('check-reset-token', [AuthController::class, 'checkResetToken']);
+    Route::post('change-password', [AuthController::class, 'changePassword']);
+});
+Route::post('fetch-user', [ProfileController::class, 'fetchUser']);
+// loggedin user routes
+// Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('login-status', [AuthController::class, 'loginStatus']);
+    Route::get('get-user', [ProfileController::class, 'getUser']);
+    Route::post('edit-user', [ProfileController::class, 'editUser']);
+// });
